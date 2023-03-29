8176b89c83514f45ad90104732c1dd4c312aadfc
emre cakir <ynsckr01@gmail.com>
2023-03-29 08:34:51 +0300

    AppRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
@@ -6,6 +5,12 @@ import Register from "../pages/Register";
 import Dashboard from "../pages/Dashboard";
 import PrivateRouter from "./PrivateRouter";
 import NotFound from "../pages/NotFound";
+import Home from "../pages/Home";
+import Brands from "../pages/Brands";
+import Purchases from "../pages/Purchases";
+import Sales from "../pages/Sales";
+import Products from "../pages/Products";
+import Firms from "../pages/Firms";
 
 const AppRouter = () => {
   return (
@@ -14,9 +19,16 @@ const AppRouter = () => {
         <Route path="/" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="stock" element={<PrivateRouter />}>
-          <Route path="" element={<Dashboard />} />
+          <Route path="" element={<Dashboard />}>
+            <Route index element={<Home />} />
+            <Route path="brands" element={<Brands />} />
+            <Route path="purchases" element={<Purchases />} />
+            <Route path="sales" element={<Sales />} />
+            <Route path="products" element={<Products />} />
+            <Route path="firms" element={<Firms />} />
+          </Route>
         </Route>
-        <Route path="*" element={<NotFound/>}/>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
