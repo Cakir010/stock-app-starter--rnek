553de159603e76e5a9b0bb3620c53d6c4393e2fc
emre cakir <ynsckr01@gmail.com>
2023-03-29 20:16:51 +0300

    son

---
+++ src/components/modals/FirmModal.jsx
@@ -0,0 +1,94 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { useState } from "react";
+import { TextField } from "@mui/material";
+import useStockCall from "../../hooks/useStockCall";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function FirmModal({ open, info, setInfo, handleClose }) {
+  const { postStockData, putStockData } = useStockCall();
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setInfo({...info, [id]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (info.id) {
+      putStockData("firms", info);
+    } else {
+      postStockData("firms", info);
+    }
+
+    handleClose();
+  };
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Box
+            sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+            component="form"
+            onSubmit={handleSubmit}
+          >
+            <TextField
+              label="Firm Name"
+              name="name"
+              id="name"
+              type="text"
+              variant="outlined"
+              onChange={handleChange}
+            />
+            <TextField
+              label="Firm Phone"
+              name="phone"
+              id="phone"
+              type="tel"
+              variant="outlined"
+              onChange={handleChange}
+            />
+            <TextField
+              label="Firm Address"
+              name="address"
+              id="address"
+              type="text"
+              variant="outlined"
+              onChange={handleChange}
+            />
+            <TextField
+              label="Image"
+              name="image"
+              id="image"
+              type="url"
+              variant="outlined"
+              onChange={handleChange}
+            />
+            <Button type="submit" variant="contained">
+              Submit Firm
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,31 +1,50 @@
 import { Button, Grid, Typography } from "@mui/material";
 import React, { useEffect } from "react";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import FirmCard from "../components/FirmCard";
+import FirmModal from "../components/modals/FirmModal";
 import useStockCall from "../hooks/useStockCall";
 import { flex } from "../styles/globalStyles";
 
 const Firms = () => {
-    const {getStockData} = useStockCall()
-    const {firms} = useSelector((state)=> state.stock)
+  const [open, setOpen] = useState(false);
 
-    useEffect(()=>{
-        getStockData('firms')
-    },[])
+  const [info, setInfo] = useState({
+    name: "",
+    phone: "",
+    address: "",
+    image: "",
+  });
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const { getStockData } = useStockCall();
+  const { firms } = useSelector((state) => state.stock);
+
+  useEffect(() => {
+    getStockData("firms");
+  }, []);
 
   return (
     <div>
       <Typography variant="h4" color="primary">
         Firm
       </Typography>
-      <Button variant="contained">New Firm</Button>
+
+      <Button variant="contained" onClick={handleOpen}>
+        New Firm
+      </Button>
+
+      <FirmModal info={info} setInfo={setInfo} open={open} handleClose={handleClose} />
+
       <Grid container sx={flex}>
-        {firms?.map((firm)=>(
-           <Grid item key={firm.id}>
-          <FirmCard  firm={firm}/>
-        </Grid> 
+        {firms?.map((firm) => (
+          <Grid item key={firm.id}>
+            <FirmCard firm={firm} />
+          </Grid>
         ))}
-        
       </Grid>
     </div>
   );
